45b1015049f5e539e2edd12fe6ea9388e14809cc
Steve Brasier <steveb@stackhpc.com>
2020-04-29 12:44:03 +0000

    IMB: uniband/biband tests

--- imb/run_imb.sh
+++ imb/run_imb.sh
@@ -3,5 +3,4 @@
 #SBATCH -n 2
 module purge
 module load gnu8/8.3.0 openmpi3/3.1.4 imb/2018.1
-mpirun --mca btl_base_warn_component_unused 0 IMB-MPI1
-
+mpirun --mca btl_base_warn_component_unused 0 IMB-MPI1 uniband biband
