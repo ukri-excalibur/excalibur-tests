99707e7d26c44786671692508d0c04e91ee8b210
Steve Brasier <steveb@stackhpc.com>
2020-05-01 11:39:24 +0000

    imb: build and run (using sbatch --wrap)

---
+++ imb-2018.1/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+# Usage:
+#  run.sh path/to/build.json
+#
+
+#set -x #echo on
+set -e # die on error
+run_config=$(realpath $1) # makes it cd-proof
+run_dir=$(dirname $run_config)
+run_label=$(basename $run_dir)
+app_label=$(basename $PWD)
+job_name="$app_label-$run_label"
+echo "Creating job: $job_name"
+build_label=$(jq -r '.build' $run_config)
+build_config=$(realpath "$run_dir/../../builds/$build_label/build.json")
+
+echo "loading modules ..."
+module purge
+module load $(jq -r '.modules.compiler' $build_config)
+module load $(jq -r '.modules.mpi' $build_config)
+module load $(jq -r '.modules.other | @sh' $build_config | tr -d "'")
+module list
+
+cd $run_dir
+mpi_opts=$(jq -r '.mpirun | @sh' $run_config | tr -d "'")
+cmdstr="mpirun $mpi_opts IMB-MPI1 uniband biband"
+sbatch_opts=$(jq -r '.sbatch | to_entries | map( "\(.key)=\(.value)") | join(" ")' $run_config)
+sbatch --job-name=$job_name $sbatch_opts --wrap="$cmdstr"
--- imb/run_imb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-set -x #echo on
-#SBATCH -N 2
-#SBATCH -n 2
-module purge
-module load gnu8/8.3.0 openmpi3/3.1.4 imb/2018.1
-mpirun --mca btl_base_warn_component_unused 0 IMB-MPI1 uniband biband
