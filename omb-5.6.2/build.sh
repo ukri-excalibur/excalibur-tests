51a3c7e4d82ec138a901d92527900fcb4fc428a6
Steve Brasier <steveb@stackhpc.com>
2020-05-03 12:31:34 +0000

    add PoC for OSU bandwidth

---
+++ omb-5.6.2/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+# Usage:
+#  build.sh path/to/build.json
+#
+
+#set -x #echo on
+set -e # die on error
+
+CONFIG=$(realpath $1) # makes it cd-proof
+VER=$(jq -r '.version' $CONFIG)
+CONF_DIR="$(dirname $1)"
+
+# check we have modules available - have to do a load otherwise can't check dependencies
+module purge
+module load $(jq -r '.modules.compiler' $CONFIG)
+module load $(jq -r '.modules.mpi' $CONFIG)
+module load $(jq -r '.modules.other | @sh' $CONFIG | tr -d "'")
+
+# if so, then nothing to do:
+echo "omb $VER installed with module, nothing to do."
