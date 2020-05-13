c8106a3e2f94cf7aa128c503eeac22f1e1ee466e
Steve Brasier <steveb@stackhpc.com>
2020-05-13 07:57:43 +0000

    changed CachedCompileOnlyTest to copy using sourcesdir

--- reframe_extras/__init__.py
+++ reframe_extras/__init__.py
@@ -10,28 +10,25 @@ from reframe.core.logging import getlogger
 import os, shutil
 
 class CachedCompileOnlyTest(rfm.CompileOnlyRegressionTest):
-    """ TODO """
+    """ NB: This manipulates `sourcesdir` in a run_before('compile') step - so don't do that in any test using this.
+        sets self.build_path to an absolute path if a preexisting build was used.
+        assumes self.executable is correct (i.e. consistent between original compile and a non-compiling run)
+    """
     @rfm.run_before('compile')
     def conditional_compile(self):
-        build_dir = os.path.join('builds', self.current_system.name, self.current_partition.name, self.current_environ.name, self.name)
+        build_dir = os.path.abspath(os.path.join('builds', self.current_system.name, self.current_partition.name, self.current_environ.name, self.name))
         build_path = os.path.join(build_dir, self.executable)
         if os.path.exists(build_path):
             self.build_path = build_path
             getlogger().info('found exe at %r', self.build_path)
             self.build_system = NoBuild()
-            # have to copy it into the stage dir else we get:
-            #  Failing phase: compile_wait
-            #  OS error: [Errno 2] No such file or directory: '/mnt/nfs/hpc-tests/stage/sausage-newslurm/compute/gnu8-openmpi3/Helloworld_Build/./Helloworld_Build'
-            dest = os.path.join(self.stagedir, self.executable)
-            shutil.copy(self.build_path, dest)
-            getlogger().info('copied to %r', dest)
-            self.sourcesdir = None # have to set this to avoid the test dir getting copied over the stage dir         
+            self.sourcesdir = build_dir # means reframe will copy the exe back in
         else:
             self.build_path = None
 
     @rfm.run_after('compile')
     def copy_executable(self):
-        if not self.build_path: # i.e. actually did a compile:
+        if not self.build_path: # i.e. only if actually did a compile:
             self.exes_dir = os.path.join('builds', self.current_system.name, self.current_partition.name, self.current_environ.name, self.name)
             if not os.path.exists(self.exes_dir):
                 os.makedirs(self.exes_dir)
