3877cea2d2cc053dacea87fcdffba1f326720e4f
Steve Brasier <steveb@stackhpc.com>
2020-07-14 13:23:57 +0100

    add max/min to IMB perf var labels and refactor from modules

--- imb/reframe_imb.py
+++ imb/reframe_imb.py
@@ -6,8 +6,6 @@
         conda activate hpc-tests
         reframe/bin/reframe -C reframe_config.py -c imb/ --run --performance-report
 
-    TODO: make latency log scale in notebook plots
-    TODO: change output read to pandas.dataframe
 """
 
 import reframe as rfm
@@ -55,8 +53,8 @@ def reduce(path, column, function):
 class IMB_PingPong(IMB_MPI1):
     METRICS = [
         # 'column' 'function', 'unit', 'label'
-        Metric('Mbytes/sec', max, 'Mbytes/sec', 'bandwidth'),
-        Metric('t[usec]', min, 't[usec]', 'latency'),
+        Metric('Mbytes/sec', max, 'Mbytes/sec', 'max_bandwidth'),
+        Metric('t[usec]', min, 't[usec]', 'min_latency'),
     ]
     def __init__(self):
         super().__init__()
@@ -65,39 +63,13 @@ class IMB_PingPong(IMB_MPI1):
         self.num_tasks_per_node = 1
         self.sanity_patterns = sn.assert_found('# Benchmarking PingPong', self.stdout)
         
-
-def ntasks_param(cpu_factor=1):
-    """ Parameterise the numbers of tasks for 2 nodes.
-    
-        Starts at 2 * (n_cpus per node) * cpu_factor then halves. Last value will always be 2.
-        Requires that all nodes have the same number of CPUs.
-
-        Returns a sequence of ints.
-    """
-    nodeinfo = modules.reframe_extras.slurm_node_info()
-    n_cpus = [int(n['CPUS']) for n in nodeinfo]
-    if not len(set(n_cpus)) == 1:
-        raise ValueError('Number of CPUs differs between nodes, cannot parameterise tasks')
-    n_cpus = n_cpus[0]
-
-    result = [int(2 * n_cpus * cpu_factor)]
-    while True:
-        v = int(result[-1] / 2)
-        if v < 2:
-            break
-        result.append(v)
-    if result[-1] != 2:
-        result.append(2)
-    return result
-
-
 n_tasks = modules.reframe_extras.ntasks_param(cpu_factor=0.5) # because alaska has HT enabled TODO: add to config?
 
 
 @rfm.parameterized_test(*[[n] for n in n_tasks]) 
 class IMB_Uniband(IMB_MPI1):
     METRICS = [
-        Metric('Mbytes/sec', max, 'Mbytes/sec', 'bandwidth')
+        Metric('Mbytes/sec', max, 'Mbytes/sec', 'max_bandwidth')
     ]
     def __init__(self, num_cpus):
         super().__init__()
@@ -110,7 +82,7 @@ class IMB_Uniband(IMB_MPI1):
 @rfm.parameterized_test(*[[n] for n in n_tasks]) 
 class IMB_Biband(IMB_MPI1):  # NB: on alaska -ib fails with a timeout!
     METRICS = [
-        Metric('Mbytes/sec', max, 'Mbytes/sec', 'bandwidth')
+        Metric('Mbytes/sec', max, 'Mbytes/sec', 'max_bandwidth')
     ]
     def __init__(self, num_cpus):
         super().__init__()
