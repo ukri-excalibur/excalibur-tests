20ce775739fc942648d078d36eb9332a2e6b0193
Steve Brasier <steveb@stackhpc.com>
2020-07-15 17:18:26 +0100

    start fixing imb for parallel tests

--- modules/imb.py
+++ modules/imb.py
@@ -14,30 +14,48 @@ import pandas as pd
 #             0         1000         2.25         0.00
 
 def read_imb_out(path):
-    """ Read stdout from an IMB-MPI1 run. Note this may only contain ONE benchmark.
+    """ Read stdout from an IMB-MPI1 run.
         
-        Returns a pandas dataframe.
+        Returns a series of pandas dataframes, one output table.
+        Each contains the columns in the output plus a column '#processes'.
     """
 
+    dframes = []
+
     COLTYPES = {
-        'uniband':(int, int, float, int),
-        'biband':(int, int, float, int),
+        'uniband': (int, int, float, int),
+        'biband': (int, int, float, int),
         'pingpong':(int, int, float, float),
     }
 
-    # process comment lines to get header:
     with open(path) as f:
-        for ix, line in enumerate(f):    
-            if line.strip().startswith('#bytes '):
-                header = line.strip().split()
-                break
-
-    # read actual data:
-    data = pd.read_csv(path, delim_whitespace=True, comment='#', names=header, header=0, skip_blank_lines=True)
-    return data
-    
+        for line in f:
+            if line.startswith('# Benchmarking '):
+                benchmark = line.split()[-1].lower()
+                converters = COLTYPES[benchmark]
+                line = next(f)
+                expect = '# #processes = '
+                if not line.startswith(expect):
+                    raise ValueError('expected %s, got %s' % (expect, nprocs_line))
+                n_procs = int(line.split('=')[-1].strip())
+                while line.startswith('#'):
+                    line = next(f) # may or may not be line ".. additional processes waiting in MPI_Barrier"
+                cols = line.strip().split()
+                rows = []
+                while True:
+                    line = next(f).strip()
+                    if line == '':
+                        break
+                    rows.append(f(v) for (f, v) in zip(converters, line.split()))
+                data = pd.DataFrame(rows, columns=cols)
+                data['#processes'] = n_procs
+                dframes.append(data)
+                #print('MAX %s: %s' % (n_procs, max(data['Mbytes/sec'])))
+    return dframes
 
 if __name__ == '__main__':
     import sys
     d = read_imb_out(sys.argv[1])
-    d = print(d)
+    for df in d:
+        print(df)
+        print()
