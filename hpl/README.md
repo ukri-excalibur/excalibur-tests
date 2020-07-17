4596587086020d2f587f5a42d643a9cbad9feafa
Steve Brasier <steveb@stackhpc.com>
2020-07-17 10:09:58 +0100

    get HPL working and extract results

--- hpl/README.md
+++ hpl/README.md
@@ -16,3 +16,9 @@ As an example, here's how the one for AlaSKA was constructed:
 - From [this](https://ulhpc-tutorials.readthedocs.io/en/latest/parallel/mpi/HPL/#hpl-main-parameters) tutorial, a block size "NB" of 192 is suggested for Broadwell processors.
 - Use [this](https://www.advancedclustering.com/act_kb/tune-hpl-dat-file/) tool with the above info to get an initial file.
 - Save this in `hpc-tests/systems/<system_name>/HPL.dat` - ReFrame will copy this to the test's staging dir before running a test
+
+
+# TODO:
+- Try different PxQ - from [here](https://community.brightcomputing.com/question/how-do-i-run-the-hpl-test-on-a-bright-cluster-5d6614ba08e8e81e885f19f3):
+    > Flat grids  like 4x1, 8x1, 4x2, etc. are good for ethernet-based networks.
+- Will only handle a single test in the output at present
--- hpl/reframe_hpl.py
+++ hpl/reframe_hpl.py
@@ -15,26 +15,42 @@ import reframe.utility.sanity as sn
 import sys, os
 sys.path.append('.')
 import modules
-from modules.reframe_extras import sequence, Scheduler_Info, CachedRunTest
+from modules.reframe_extras import Scheduler_Info, CachedRunTest
 
-node_seq = [2] # [Scheduler_Info().num_nodes]  TODO: make it run on all nodes
-
-@rfm.parameterized_test(*[[n] for n in node_seq])
+@rfm.simple_test
 class Hpl(rfm.RunOnlyRegressionTest, CachedRunTest):
     """ Run on all physical cores of the specified number of nodes """
     
-    def __init__(self, num_nodes):
+    def __init__(self):
         self.valid_systems = ['*']
         self.valid_prog_environs = ['hpl']
         
         self.executable = 'xhpl'
         self.num_tasks_per_node = Scheduler_Info().pcores_per_node
-        self.num_tasks = num_nodes * self.num_tasks_per_node
+        self.num_tasks = Scheduler_Info().num_nodes * self.num_tasks_per_node
         self.exclusive_access = True
         
+        # this copies HPL.dat to staging dir:
         self.sourcesdir = os.path.join('..', 'systems', self.current_system.name, 'hpl') # TODO: optionally support partitions here?
 
         self.use_cache = False # set to True to use cached results for debugging postprocessing of results
         
-        self.sanity_patterns = sn.assert_found('# TODO', self.stdout)
+        self.sanity_patterns = sn.all([
+            sn.assert_found('End of Tests.', self.stdout),
+            sn.assert_found('0 tests completed and failed residual checks', self.stdout),
+            sn.assert_found('0 tests skipped because of illegal input values.', self.stdout)
+        ])
+        self.perf_patterns = {
+            # e.g.:
+            # T/V                N    NB     P     Q               Time                 Gflops
+            # --------------------------------------------------------------------------------
+            # WR11C2R4       46080   192    16    32              12.91             5.0523e+03
+
+            # see hpl-2.3/testing/ptest/HPL_pdtest.c:{219,253-256} for pattern details
+            'Gflops': sn.extractsingle(r'^W[R|C]\S+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d[\d.]+\s+(\d[\d.eE+]+)', self.stdout, 1, float)
+        }
+        self.reference = {
+            '*': {'Gflops': (None, None, None, 'Gflops')}
+        }
+
         
