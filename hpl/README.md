29df1d45b72c5787578db86bfe68d9f7298ba4cc
Steve Brasier <steveb@stackhpc.com>
2020-07-17 13:40:09 +0100

    run HPL on one and all nodes

--- hpl/README.md
+++ hpl/README.md
@@ -1,21 +1,32 @@
+# High Performance Linpack
 
+Run HPL tests on one and all nodes.
 
 # Installation using Spack
 
-E.g.
+E.g.:
+
     spack load patch
     spack install hpl ^/ziwdzwh # openmpi dependency
 
+# HPL .dat configuration files
 
-# Generating an HPL configuration
+Appropriate HPL configuration files named `HPL-single.dat` and `HPL-all.dat` for the single- and all-node cases respectively must be generated and placed in either:
 
-As an example, here's how the one for AlaSKA was constructed:
-- The OpenHPC cluster had 16 compute nodes.
-- Use `srun --pty bash -i` to get to a compute node and run `/proc/cpuinfo` to get processor info: This shows E5-2683 v4 CPUs, i.e. Broadwell.
-- Use `sinfo -N --long` to get other processor info, with the "S:C:T" field showing 32 physical cores per node.
+- `<repo_root>/systems/<sysname>/hpl/`
+- `<repo_root>/systems/<sysname>/<partition>/hpl/`
+
+depending on whether system-wide or partition-specific configurations are required. ReFrame will copy these files into the staging directories before running a test, so changes made to these files will persist and apply to the next run.
+
+As an example, the configuration files for AlaSKA were generated as follows:
+- Run `sinfo -N --long` to get:
+    - The number of nodes for the "all" case (16)
+    - The number of physical cores per node (32) from the "S:C:T" field (S * C).
+- Use Horizon to get memory per node (128GB)
+- Run `srun --pty bash -i` to get a shell on a compute node and run `/proc/cpuinfo` to get processor type and therefore microarchitecture (E5-2683 v4, i.e. Broadwell).
 - From [this](https://ulhpc-tutorials.readthedocs.io/en/latest/parallel/mpi/HPL/#hpl-main-parameters) tutorial, a block size "NB" of 192 is suggested for Broadwell processors.
-- Use [this](https://www.advancedclustering.com/act_kb/tune-hpl-dat-file/) tool with the above info to get an initial file.
-- Save this in `hpc-tests/systems/<system_name>/HPL.dat` - ReFrame will copy this to the test's staging dir before running a test
+- Use [this](https://www.advancedclustering.com/act_kb/tune-hpl-dat-file/) tool with the above info to get initial configuration files.
+- Place generated files in the above directories.
 
 
 # TODO:
--- hpl/reframe_hpl.py
+++ hpl/reframe_hpl.py
@@ -1,6 +1,6 @@
 """ High Performance Linpack.
 
-    TODO: Describe where it runs.
+    Run on all physical cores of a) 1 and b) all nodes
 
     Run using e.g.:
         
@@ -8,31 +8,35 @@
         conda activate hpc-tests
         reframe/bin/reframe -C reframe_config.py -c hpl/ --run --performance-report
 
+    Requires files HPL-{single, all}.dat in one of:
+        - <repo_root>/systems/<sysname>/hpl/
+        - <repo_root>/systems/<sysname>/<partition>/hpl/
 """
 
 import reframe as rfm
 import reframe.utility.sanity as sn
-import sys, os
+import sys, os, shutil
 sys.path.append('.')
 import modules
 from modules.reframe_extras import Scheduler_Info, CachedRunTest
 
-@rfm.simple_test
+@rfm.parameterized_test(*[['single'], ['all']])
 class Hpl(rfm.RunOnlyRegressionTest, CachedRunTest):
-    """ Run on all physical cores of the specified number of nodes """
+    """ See module docstring """
     
-    def __init__(self):
+    def __init__(self, size):
+        
         self.valid_systems = ['*']
         self.valid_prog_environs = ['hpl']
         
-        self.executable = 'xhpl'
+        self.size = size
+        num_nodes = {'single':1, 'all':Scheduler_Info().num_nodes}[size]        
         self.num_tasks_per_node = Scheduler_Info().pcores_per_node
-        self.num_tasks = Scheduler_Info().num_nodes * self.num_tasks_per_node
+        self.num_tasks = num_nodes * self.num_tasks_per_node
         self.exclusive_access = True
+        self.executable = 'xhpl'
+        self.time_limit = None
         
-        # this copies HPL.dat to staging dir:
-        self.sourcesdir = os.path.join('..', 'systems', self.current_system.name, 'hpl') # TODO: optionally support partitions here?
-
         self.use_cache = False # set to True to use cached results for debugging postprocessing of results
         
         self.sanity_patterns = sn.all([
@@ -53,4 +57,24 @@ class Hpl(rfm.RunOnlyRegressionTest, CachedRunTest):
             '*': {'Gflops': (None, None, None, 'Gflops')}
         }
 
-        
+    @rfm.run_before('run')
+    def copy_input(self):
+        """ Copy the appropriate .dat file into staging.
+
+            Can't just set sourcesdir as we only want one file.
+        """
+        dat_file = 'HPL-%s.dat' % self.size
+        dat_source = None
+        prefix = os.path.join(self.prefix, '..', 'systems', self.current_system.name) # use self.prefix so relative to test dir, not pwd
+        suffixes = ['', self.current_partition.name]
+        locations = [os.path.abspath(os.path.join(prefix, suffix, 'hpl', dat_file)) for suffix in suffixes]
+        for location in locations:
+            if os.path.exists(location):
+                dat_source = location
+        if dat_source is None:
+            raise ValueError('.dat file not found in %s' % ' or '.join(locations))
+        else:
+            dat_dest = os.path.join(self.stagedir, 'HPL.dat')
+            #raise NotImplementedError((dat_source, dat_dest))
+            shutil.copy(dat_source, dat_dest)
+            
