4ff7c3de5d8354951d8ae4ed0f8a1ca46a1ed0d2
Steve Brasier <steveb@stackhpc.com>
2020-08-11 14:02:28 +0100

    swap openfoam motorbike to larger semi-official benchmark

--- openfoam/reframe_openfoam.py
+++ openfoam/reframe_openfoam.py
@@ -25,18 +25,6 @@ from modules.reframe_extras import sequence, Scheduler_Info, CachedRunTest
 
 node_seq = sequence(1, Scheduler_Info().num_nodes + 1, 2)
 
-def parse_time(s):
-    """ Convert timing info from `time` into float seconds.
-
-       E.g. parse_time('0m0.000s') -> 0.0
-    """
-
-    mins, _, secs = s.partition('m')
-    mins = float(mins)
-    secs = float(secs.rstrip('s'))
-
-    return mins * 60.0 + secs
-
 @rfm.parameterized_test(*[[n_nodes] for n_nodes in node_seq])
 class Openfoam_Mbike(rfm.RunOnlyRegressionTest):
 
@@ -52,36 +40,38 @@ class Openfoam_Mbike(rfm.RunOnlyRegressionTest):
         self.num_tasks_per_node = Scheduler_Info().pcores_per_node
         self.num_tasks = self.num_nodes * self.num_tasks_per_node
         
-        #self.sourcesdir = '$FOAM_TUTORIALS'
+        self.sourcesdir = 'downloads'
         self.exclusive_access = True
         self.time_limit = None
 
-        # NB: the pre_run commands actually do the MPI runs here
         self.pre_run = [
-            'cp -r $FOAM_TUTORIALS/incompressible/simpleFoam/motorBike/* .', # FOAM_TUTORIALS set by module
-            './Allclean', # removes logs, old timehistories etc
+            'tar --strip-components 2 -xf Motorbike_bench_template.tar.gz bench_template/basecase',
+            './Allclean', # removes logs, old timehistories etc just in case 
             
             # set domain decomposition:
             # using 'scotch' method means simpleCoeffs is ignored so it doesn't need to match num_tasks:
             'sed -i -- "s/method .*/method          scotch;/g" system/decomposeParDict',
             'sed -i -- "s/numberOfSubdomains .*/numberOfSubdomains %i;/g" system/decomposeParDict' % self.num_tasks,
 
-            'time ./Allrun', # actually runs steps of analysis
+            # remove streamlines:
+            'sed -i -- \'s/    #include "streamLines"//g\' system/controlDict',
+            'sed -i -- \'s/    #include "wallBoundedStreamLines"//g\' system/controlDict',
+
+            # fix location of mesh quality defaults (needed for v6+?)
+            "sed -i -- 's|caseDicts|caseDicts/mesh/generation|' system/meshQualityDict",
+
+            'time ./Allmesh',
         ]
         # could also check:
         #$ ompi_info -c | grep -oE "MPI_THREAD_MULTIPLE[^,]*"
         # MPI_THREAD_MULTIPLE: yes
 
-        # define a no-op to keep reframe happy:
-        self.executable = 'hostname'
-        self.executable_opts = []
+        self.executable = 'simpleFoam' # Duh this runs srun time - 
+        self.executable_opts = ['-parallel']
         
         self.post_run = []
 
-        self.keep_files = [
-            'log.potentialFoam', 'log.snappyHexMesh', 'log.blockMesh', 'log.reconstructPar', 'log.surfaceFeatures',
-            'log.decomposePar', 'log.reconstructParMesh', 'log.patchSummary', 'log.simpleFoam',
-                  ]
+        self.keep_files = ['log.snappyHexMesh', 'log.blockMesh', 'log.decomposePar']
 
         result = sn.extractall(
             r'time step continuity errors : '
@@ -89,9 +79,16 @@ class Openfoam_Mbike(rfm.RunOnlyRegressionTest):
             self.stdout, 'res', float)
         # NB: `time` outputs to stderr so can't assume that should be empty
         self.sanity_patterns = sn.all([
-            # ensure simpleFoam finished:
-            sn.assert_found('Finalising parallel run', 'log.simpleFoam'),
-            sn.assert_not_found('FOAM FATAL ERROR', 'log.simpleFoam'),
+            # ensure meshing finished ok:
+            sn.assert_found('End', 'log.blockMesh'),
+            sn.assert_found('End', 'log.decomposePar'),
+            sn.assert_found('Finished meshing without any errors', 'log.snappyHexMesh'),
+            
+            # ensure simpleFoam finished ok:
+            sn.assert_found('Finalising parallel run', self.stdout),
+            sn.assert_not_found('FOAM FATAL ERROR', self.stdout),
+            sn.assert_not_found('FOAM FATAL ERROR', self.stderr),
+            
             # ensure continuity errors small enough - copied from
             # https://github.com/eth-cscs/reframe/blob/0a4dc5207b35c737861db346bd483fd4ac202846/cscs-checks/apps/openfoam/check_openfoam_extend.py#L56
             sn.all(
@@ -100,7 +97,7 @@ class Openfoam_Mbike(rfm.RunOnlyRegressionTest):
         ])
 
         self.perf_patterns = {
-            'Allrun_realtime': sn.extractsingle(r'^real\s+(\d+m[\d.]+s)$', self.stderr, 1, parse_time),
+            'Allrun_realtime': sn.extractall(r'ExecutionTime = ([\d.]+) s', self.stdout, 1, float)[-1],
         }
         self.reference = {
             '*': {
