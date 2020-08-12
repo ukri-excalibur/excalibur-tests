70b2d93caaa591d28967713f1f6116296d220c1c
Steve Brasier <steveb@stackhpc.com>
2020-08-12 12:08:32 +0100

    add time info to cp2k

--- apps/cp2k/reframe_cp2k.py
+++ apps/cp2k/reframe_cp2k.py
@@ -29,6 +29,7 @@ from collections import namedtuple
 from reframe.core.logging import getlogger
 sys.path.append('.')
 from modules.reframe_extras import sequence, Scheduler_Info, CachedRunTest
+from modules.utils import parse_time_cmd
 # want to use a module supplied as part of the reframe git repo:
 RFM_CP2K_PATH = os.path.join('reframe', 'cscs-checks', 'apps', 'cp2k')
 sys.path.append(RFM_CP2K_PATH)
@@ -48,6 +49,7 @@ class Cp2k_H2O_256(Rfm_Cp2kCheck):
         self.valid_prog_environs = ['cp2k']
         self.modules = []
         self.extra_resources = {}
+        self.pre_run = ['time \\']
         self.executable = 'cp2k.popt'
         self.sourcesdir = os.path.join(os.path.abspath(RFM_CP2K_PATH), 'src')
 
@@ -61,4 +63,22 @@ class Cp2k_H2O_256(Rfm_Cp2kCheck):
         self.exclusive_access = True
         self.time_limit = None
         
-        # all sanity/performance patterns defined in superclass
+        # sanity patterns defined in superclass
+
+        self.perf_patterns = {
+            # from cp2k output:
+            'cp2k_time': sn.extractsingle(r'^ CP2K\s+\d+\s+[\d.]+\s+[\d.]+\s+[\d.]+\s+[\d.]+\s+([\d.]+)',
+                                     self.stdout, 1, float), # "Total Max" time for CP2K subroutine
+            # from `time`:
+            'runtime_real': sn.extractsingle(r'^real\s+(\d+m[\d.]+s)$', self.stderr, 1, parse_time_cmd),
+            'runtime_user': sn.extractsingle(r'^user\s+(\d+m[\d.]+s)$', self.stderr, 1, parse_time_cmd),
+            'runtime_sys': sn.extractsingle(r'^sys\s+(\d+m[\d.]+s)$', self.stderr, 1, parse_time_cmd),
+        }
+        self.reference = {
+            '*': {
+                'cp2k_time': (0, None, None, 's'),
+                'runtime_real': (0, None, None, 's'),
+                'runtime_user': (0, None, None, 's'),
+                'runtime_sys': (0, None, None, 's'),
+            }
+        }
