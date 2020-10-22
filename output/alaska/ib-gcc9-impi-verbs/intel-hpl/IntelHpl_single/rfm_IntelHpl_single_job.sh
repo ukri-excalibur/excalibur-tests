37bbdc95a25bc53f06b8248ab3f515923655b71f
Steve Brasier <steveb@stackhpc.com>
2020-10-22 13:01:36 +0100

    add alaska HPL runs on IB

---
+++ output/alaska/ib-gcc9-impi-verbs/intel-hpl/IntelHpl_single/rfm_IntelHpl_single_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name="rfm_IntelHpl_single_job"
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=rfm_IntelHpl_single_job.out
+#SBATCH --error=rfm_IntelHpl_single_job.err
+#SBATCH --time=1:0:0
+#SBATCH --exclusive
+module load gcc/9.3.0-5abm3xg
+module load intel-mpi/2019.7.217-bzs5ocr
+export FI_VERBS_IFACE=ib
+module load intel-mkl/2020.1.217-5tpgp7b
+mpirun -np 1 $MKLROOT/benchmarks/mp_linpack/xhpl_intel64_dynamic
