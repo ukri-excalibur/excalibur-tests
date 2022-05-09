0bb4443c6355d2af0bfb207dbecc45cb7db1b193
Tom Young <39765193+t-young31@users.noreply.github.com>
2022-05-09 12:17:02 +0100

    Add Grid (#23)
    * Add Grid
    
    * Add readme and reference values
    
    * Extract value with regex
    
    * Allow all environments
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ apps/grid/reframe_grid.py
@@ -0,0 +1,60 @@
+"""
+ReFrame benchmark for GIRD: https://github.com/paboyle/Grid
+
+Assumes a GRID install is present on the system. Developed from
+https://github.com/DiRAC-HPC/es-benchmarking-public
+
+WARNING:
+The code is Hybrid OpenMP + MPI with NUMA socket aware optimisations.
+The relevant options can make big changes to delivered performance.
+"""
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+@rfm.simple_test
+class GridBenchmark(rfm.RunOnlyRegressionTest):
+
+    valid_systems = ['*']
+    valid_prog_environs = ['*']
+
+    num_tasks = 1
+    num_tasks_per_node = 1
+    num_cpus_per_task = 20
+
+    executable = 'Benchmark_ITT'
+    executable_opts = ['--shm 1024 --shm-hugetlb']
+    time_limit = '20m'
+    variables = {
+        'OMP_NUM_THREADS': f'{num_cpus_per_task}',
+    }
+    extra_resources = {
+        'mpi': {'num_slots': num_tasks * num_cpus_per_task}
+    }
+
+    reference = {
+        'csd3:icelake': {
+            'Performance': (228286, None, None, 'Mflop/s per node')
+        },
+        '*': {
+            'Performance': (150000, None, None, 'Mflop/s per node'),
+        }
+    }
+
+    @run_before('sanity')
+    def set_sanity_patterns(self):
+        self.sanity_patterns = sn.assert_found(r'Per Node Summary table',
+                                               self.stdout)
+
+    @run_before('performance')
+    def set_perf_patterns(self):
+        """
+        e.g.
+
+        Grid : Message : 380809 ms :  Comparison point  result: 143382.7 Mflop/s per node
+        """
+
+        self.perf_patterns = {
+            'Performance':
+                sn.extractsingle('result: (\S+) Mflop/s per node', self.stdout, 1, float)
+                }
