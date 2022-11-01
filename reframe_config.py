54316438c9570bf2844207b369c2e43a12784ebb
Mosè Giordano <mose@gnu.org>
2022-11-01 17:29:25 +0000

    [isambard] Add A64FX configuration and split systems

--- reframe_config.py
+++ reframe_config.py
@@ -132,12 +132,12 @@ site_configuration = {
         },  # end Myriad
         {
             # https://gw4-isambard.github.io/docs/user-guide/MACS.html
-            'name': 'isambard',
+            'name': 'isambard-cascadelake',
             'descr': 'Cascade Lake nodes of Isambard 2',
             'hostnames': ['login-0[12].gw4.metoffice.gov.uk'],
             'partitions': [
                 {
-                    'name': 'cascadelake',
+                    'name': 'compute-node',
                     'descr': 'Cascadelake computing nodes',
                     'scheduler': 'pbs',
                     'launcher': 'mpirun',
@@ -151,6 +151,14 @@ site_configuration = {
                         'num_cpus_per_socket': 20,
                     },
                 },
+            ]
+        },  # end Isambard Cascadelake
+        {
+            # https://gw4-isambard.github.io/docs/user-guide/A64FX.html
+            'name': 'isambard-a64fx',
+            'descr': 'A64FX nodes of Isambard 2',
+            'hostnames': ['gw4a64fxlogin[0-9]+'],
+            'partitions': [
                 {
                     'name': 'a64fx',
                     'descr': 'A64FX computing nodes',
@@ -167,7 +175,7 @@ site_configuration = {
                     },
                 },
             ]
-        },  # end Isambard Cascadelake
+        },  # end Isambard A64FX
         {
             # https://www.dur.ac.uk/icc/cosma/support/cosma8/
             'name': 'cosma8',
