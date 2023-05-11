e6f55b7c2e8e8f75fb9aebce69f81e14895a5af2
Kaan Olgu <k.olgu20@gmail.com>
2023-05-11 13:20:07 +0000

    [babelstream] ADD : Performance Report Functions

--- benchmarks/apps/babelstream/babelstream.py
+++ benchmarks/apps/babelstream/babelstream.py
@@ -7,7 +7,7 @@ import os.path as path
 import sys
 import reframe as rfm
 import reframe.utility.sanity as sn
-
+from reframe.core.backends import getlauncher
 # Add top-level directory to `sys.path` so we can easily import extra modules
 # from any directory.
 sys.path.append(path.join(path.dirname(__file__), '..', '..'))
@@ -16,21 +16,105 @@ sys.path.append(path.join(path.dirname(__file__), '..', '..'))
 # attribute.
 from modules.utils import SpackTest
 
-@rfm.simple_test
-class BabelstreamBenchmark(SpackTest):
-    descr = 'Demo test using Spack to build the test code'
-    valid_systems = ['*']
-    valid_prog_environs = ['builtin']
-    executable = 'babelstream'
-    executable_opts = ['--help']
-    build_system = 'Spack'
+
 
 # spack install --add babelstream%gcc@9.2.0 +thrust backend=cuda cuda_arch=70
-
+@rfm.simple_test
+class BabelstreamBenchmarkBase(SpackTest):
+    descr = 'Build BabelStream with Spack Build System'
+    valid_systems = ['*']
+    valid_prog_environs = ['default'] 
+    # executable = 'babelstream'
+    # executable_opts = ['--help']
+    spack_spec = 'babelstream@develop'
+    # spack_env = variable(str, value='/default/dir')
+    
+        
     @run_before('compile')
     def setup_build_system(self):
-        self.build_system.specs = ['babelstream@develop%gcc@9.2.0 +thrust implementation=cuda cuda_arch=70']
+        self.build_system.specs = [self.spack_spec] 
+        # print(self.build_system.environment)
+        # self.build_system.environment = self.spack_env
 
-    @sanity_function
-    def assert_version(self):
-        return sn.assert_found(r'Version 4.0.4', self.stderr)
+    @run_before('run')
+    def replace_launcher(self):
+        # For this benchmark we don't use MPI at all, so we always force the
+        # local launcher:
+        # <https://reframe-hpc.readthedocs.io/en/v4.1.3/tutorial_advanced.html#replacing-the-parallel-launcher>.
+        self.job.launcher = getlauncher('local')()
+    
+    @run_after('setup')
+    def setup_variables(self):
+        # self.executable_opts = [os.path.join(self.prefix, 'openmm-gpu-bench.py')]
+        # self.set_var_default(
+        #     'num_cpus_per_task',
+        #     self.current_partition.processor.num_cpus)
+        # self.env_vars['OMP_NUM_THREADS'] = f'{self.num_cpus_per_task}'
+        # self.env_vars['OMP_PLACES'] = 'cores'
+        # # Needed for the SGE scheduler
+        # self.extra_resources['mpi'] = {'num_slots': self.num_tasks * self.num_cpus_per_task}
+        # Request the GPU resources necessary to run this job.
+        self.extra_resources['gpu'] = {'num_gpus_per_node': self.num_gpus_per_node}
+
+    @run_before('sanity')
+    def set_sanity_patterns(self):
+        self.sanity_patterns = sn.assert_found('Running kernels 100 times', self.stdout)
+
+#    @run_before('performance')
+#     def set_perf_patterns(self):
+#         self.perf_patterns = {
+#             'speed': sn.extractsingle(
+#                 r'^100\.0%(?:[ \t]+[-\d.]+){5}[ \t]+([-\d.]+)[ \t]+[\d:]+$',
+#                 self.stdout, 1, float),
+#         }
+
+# Python's built-in float type has double precision (it's a C double in CPython, a Java double in Jython).
+# If you need more precision, get NumPy and use its numpy.float128.
+    @performance_function('MBytes/sec', perf_key='Copy')
+    def extract_copy_perf(self):
+        return sn.extractsingle(r'Copy \s+(\S+)\s+.', self.stdout, 1, float)
+
+    @performance_function('MBytes/sec', perf_key='Mul')
+    def extract_scale_perf(self):
+        return sn.extractsingle(r'Mul \s+(\S+)\s+..', self.stdout, 1, float)
+
+    @performance_function('MBytes/sec', perf_key='Add')
+    def extract_add_perf(self):
+        return sn.extractsingle(r'Add \s+(\S+)\s+.?', self.stdout, 1, float)
+
+    @performance_function('MBytes/sec', perf_key='Triad')
+    def extract_triad_perf(self):
+        return sn.extractsingle(r'Triad \s+(\S+)\s+.', self.stdout, 1, float)
+    @performance_function('MBytes/sec', perf_key='Dot')
+    def extract_dot_perf(self):
+        return sn.extractsingle(r'Dot \s+(\S+)\s+.', self.stdout, 1, float)
+
+
+@rfm.simple_test
+class ACCBenchmark(BabelstreamBenchmarkBase):
+    valid_systems = ['*']
+    tags = {"acc"}
+    executable = "acc-stream"
+    # modules=['cudatoolkit']
+    # environment = "~/excalibur-tests/benchmarks/spack/isambard-macs/cascadelake/"
+
+
+@rfm.simple_test
+class CUDABenchmark(BabelstreamBenchmarkBase):
+    valid_systems = ['*']
+    tags = {"cuda"}
+    executable = "cuda-stream"
+    valid_systems = ['+gpu']
+    num_gpus_per_node = 1
+    # environment = "~/excalibur-tests/benchmarks/spack/isambard-macs/volta"
+
+#     @run_after('init')
+#     def set_up_from_parameters(self):
+#         # self.executable_opts = ['-l', '8x4x4x4', '-p', '2x1x1x1']
+#         # self.num_tasks = 2
+#         # self.extra_resources = {'mpi': {'num_slots': self.num_tasks}}
+
+    
+
+
+
