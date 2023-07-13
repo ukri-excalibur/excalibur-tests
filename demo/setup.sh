21b674d679c2b6e3fe3221837ff3a521e2e65975
Tuomas Koskela <t.koskela@ucl.ac.uk>
2023-07-13 13:47:21 +0100

    Add system specifics to setup

--- demo/setup.sh
+++ demo/setup.sh
@@ -1,6 +1,28 @@
 #!/bin/bash -l
 
+system=$1
+
+# System specific part of setup. Mostly load the correct python module
+if [ $system == archer2 ]
+then
+    module load cray-python
+    cd /work/d193/d193/tk-d193
+elif [ $system == csd3 ]
+then
+    module load python/3.8
+elif [ $system == cosma ]
+then
+    module swap python/3.10.7
+elif [ $system == isambard ]
+then
+    # on isambard we have python 3.7 by default which should be enough
+    true
+fi
+
 # Setup
+mkdir demo
+cd demo
+python3 --version
 python3 -m venv demo-env
 source ./demo-env/bin/activate
 git clone git@github.com:ukri-excalibur/excalibur-tests.git
@@ -8,8 +30,6 @@ git clone -c feature.manyFiles=true git@github.com:spack/spack.git
 source ./spack/share/spack/setup-env.sh
 export RFM_CONFIG_FILES="$(pwd)/excalibur-tests/benchmarks/reframe_config.py"
 export RFM_USE_LOGIN_SHELL="true"
-python3 --version
-pip --version
 pip install --upgrade pip
 pip install ./excalibur-tests
 
