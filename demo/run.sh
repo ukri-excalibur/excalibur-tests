1743b61a7cb005594459d2bdd54218d6070edf48
Tuomas Koskela <t.koskela@ucl.ac.uk>
2023-07-13 13:32:54 +0100

    Add run options

--- demo/run.sh
+++ demo/run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 app=$1
-system=$2
+spec=$2
+system=$3
+
+apps_dir=excalibur-tests/benchmarks/apps
 
 if [ $system == archer2 ]
 then
-    reframe -c benchmarks/apps/$app -r -J'--qos=short' --system archer2
+    reframe -c $apps_dir/$app -r -J'--qos=short' --system archer2 -S spack_spec=$spec
 elif [ $system == cosma ]
 then
-    reframe -c benchmarks/apps/$app -r -J'--account=do006' --system cosma8
+    reframe -c $apps_dir/$app -r -J'--account=do006' --system cosma8 -S spack_spec=$spec
 elif [ $system == csd3 ]
 then
-    reframe -c benchmarks/apps/$app -r -J'--account=DIRAC-DO006-CPU' --system csd3-skylake
+    reframe -c $apps_dir/$app -r -J'--account=DIRAC-DO006-CPU' --system csd3-skylake -S spack_spec=$spec
 elif [ $system == isambard ]
 then
-    reframe -c benchmarks/apps/$app -r --system isambard-macs:cascadelake -S build_locally=false
+    reframe -c $apps_dir/$app -r --system isambard-macs:cascadelake -S build_locally=false -S spack_spec=$spec
 fi
