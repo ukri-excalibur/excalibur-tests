3bbf963b45da19e8b76c8a679ce1db0a9594debb
Tuomas Koskela <t.koskela@ucl.ac.uk>
2023-07-14 12:22:17 +0100

    Run on 4 nodes, remove redundant args

--- demo/run.sh
+++ demo/run.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 app=$1
-spec=$2
+compiler=$2
 system=$3
+spec=$app\%$compiler
 
 apps_dir=excalibur-tests/benchmarks/apps
 
 if [ $system == archer2 ]
 then
-    reframe -c $apps_dir/$app -r -J'--qos=short' --system archer2 -S spack_spec=$spec
+    reframe -c $apps_dir/$app -r -J'--qos=short' --system archer2 -S spack_spec=$spec --setvar=num_cpus_per_task=8  --setvar=num_tasks_per_node=2 --setvar=num_tasks=8
 elif [ $system == cosma ]
 then
-    reframe -c $apps_dir/$app -r -J'--account=do006' --system cosma8 -S spack_spec=$spec
+    reframe -c $apps_dir/$app -r -J'--account=do006' --system cosma8 -S spack_spec=$spec --setvar=num_cpus_per_task=8  --setvar=num_tasks_per_node=2 --setvar=num_tasks=8
 elif [ $system == csd3 ]
 then
-    reframe -c $apps_dir/$app -r -J'--account=DIRAC-DO006-CPU' --system csd3-skylake -S spack_spec=$spec
+    reframe -c $apps_dir/$app -r -J'--account=DIRAC-DO006-CPU' --system csd3-cascadelake -S spack_spec=$spec --setvar=num_cpus_per_task=8  --setvar=num_tasks_per_node=2 --setvar=num_tasks=8
 elif [ $system == isambard ]
 then
-    reframe -c $apps_dir/$app -r --system isambard-macs:cascadelake -S build_locally=false -S spack_spec=$spec
+    reframe -c $apps_dir/$app -r --system isambard-macs:cascadelake -S build_locally=false -S spack_spec=$spec --setvar=num_cpus_per_task=8  --setvar=num_tasks_per_node=2 --setvar=num_tasks=8
 fi
