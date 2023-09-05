8ff47b3c1e1e3bb78dfab19c96729f3bade50ab8
Tuomas Koskela <t.koskela@ucl.ac.uk>
2023-09-05 16:49:26 +0100

    Remove retired skylake partition, update urls

--- benchmarks/reframe_config.py
+++ benchmarks/reframe_config.py
@@ -66,31 +66,7 @@ site_configuration = {
             ]
         },  # end ARCHER2
         {
-            # https://www.hpc.cam.ac.uk/systems/peta-4
-            'name': 'csd3-skylake',
-            'descr': 'CSD3 Skylake',
-            'hostnames': ['login-e-[0-9]+'],
-            'modules_system': 'tmod32',
-            'partitions': [
-                {
-                    'name': 'compute-node',
-                    'descr': 'Skylake compute nodes',
-                    'scheduler': 'slurm',
-                    'launcher': 'mpirun',
-                    'access': ['--partition=skylake'],
-                    'environs': ['default'],
-                    'max_jobs': 64,
-                    'processor': {
-                        'num_cpus': 32,
-                        'num_cpus_per_core': 2,
-                        'num_sockets': 2,
-                        'num_cpus_per_socket': 16,
-                    }
-                },
-            ]
-        },  # end CSD3 Skylake
-        {
-            # https://www.hpc.cam.ac.uk/systems/peta-4
+            # https://docs.hpc.cam.ac.uk/hpc/user-guide/cclake.html
             'name': 'csd3-icelake',
             'descr': 'CSD3 Icelake',
             'hostnames': ['login-q-[0-9]+'],
@@ -114,7 +90,7 @@ site_configuration = {
             ]
         },  # end CSD3 Icelake
         {
-            # https://www.hpc.cam.ac.uk/systems/peta-4
+            # https://docs.hpc.cam.ac.uk/hpc/user-guide/cclake.html
             'name': 'csd3-cascadelake',
             'descr': 'CSD3 Cascade lake',
             'hostnames': ['login-e-[0-9]+'],
