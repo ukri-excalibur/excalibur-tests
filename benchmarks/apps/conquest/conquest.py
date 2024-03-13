06edb702ff3dd6e6acb226a1bc724445b005dcf4
Tuomas Koskela <t.koskela@ucl.ac.uk>
2024-03-13 12:43:59 +0000

    Add larger benchmarks, fix init of SiWeakScaling

--- benchmarks/apps/conquest/conquest.py
+++ benchmarks/apps/conquest/conquest.py
@@ -67,27 +67,41 @@ class SiWeakScaling(ConquestBaseBenchmark):
     tags = {"si_weakscaling"}
     num_threads = parameter([1,2,4,8])
     num_cores = parameter([8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768])
+    #num_cores = parameter([8])
+    num_atoms_per_core = parameter([8])
 
-    def __init__(self):
+    @run_after('setup')
+    def setup_variables(self):
+
         input_files = {
-            8: "si_222.xtl",
-            16: "si_422.xtl",
-            32: "si_442.xtl",
-            64: "si_444.xtl",
-            128: "si_844.xtl",
-            256: "si_884.xtl",
-            512: "si_888.xtl",
-            1024: "si_1688.xtl",
-            2048: "si_16168.xtl",
-            4096: "si_161616.xtl",
-            8192: "si_321616.xtl",
-            16384: "si_323216.xtl",
-            32768: "si_323232.xtl"}
+            2**6: "si_222.xtl",
+            2**7: "si_422.xtl",
+            2**8: "si_442.xtl",
+            2**9:  "si_444.xtl",
+            2**10: "si_844.xtl",
+            2**11: "si_884.xtl",
+            2**12:"si_888.xtl",
+            2**13: "si_1688.xtl",
+            2**14: "si_16168.xtl",
+            2**15: "si_161616.xtl",
+            2**16: "si_321616.xtl",
+            2**17: "si_323216.xtl",
+            2**18: "si_323232.xtl"}
         input_dir = "$(dirname $(which Conquest))/../benchmarks/matrix_multiply"
 
-        self.num_tasks = int(self.num_cores / self.num_threads)
+        self.num_tasks = max(1, self.num_cores // self.num_threads)
         self.num_cpus_per_task = self.num_threads
+        self.num_tasks_per_node = max(1, min(self.num_tasks, self.current_partition.processor.num_cpus // self.num_threads))
+        self.env_vars['OMP_NUM_THREADS'] = f'{self.num_cpus_per_task}'
 
-        self.prerun_cmds.append(f"cp {input_dir}/{input_files[self.num_cores]} ./coords.dat")
+        self.prerun_cmds.append(f"cp {input_dir}/{input_files[self.num_cores*self.num_atoms_per_core]} ./coords.dat")
         self.prerun_cmds.append(f"cp {input_dir}/Conquest_input .")
         self.prerun_cmds.append(f"cp {input_dir}/Si.ion .")
+
+        self.time_limit = "2h"
+
+    # Performance tuning by passing system specific scheduler options
+    @run_before('run')
+    def set_cpu_binding(self):
+        if self.current_system.name == 'archer2':
+            self.job.launcher.options = ['--distribution=block:block --hint=nomultithread']
