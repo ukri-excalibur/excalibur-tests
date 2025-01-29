cdea8af6b9c21c1bf88b7034466b7f1376e8e1d4
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-01-29 16:45:16 +0000

    [ziatest] Allow passing `num_tasks_per_node` from the command line

--- benchmarks/apps/ziatest/ziatest.py
+++ benchmarks/apps/ziatest/ziatest.py
@@ -10,17 +10,22 @@ class ZiatestBenchmark(SpackTest):
     spack_spec = 'ziatest'
     num_tasks = 1
     num_tasks_per_node = 1
-    cpus_per_task = 1
     # We don't call `ziatest` but do the call to `ziaprobe` directly, as we
     # better control the MPI launcher and its arguments.
     executable = 'ziaprobe'
-    executable_opts = [
-        # This requires using coreutil's `date`, but that
-        # should be available on most clusters.
-        '$(date +"%s %6N")',
-        str(num_tasks),
-    ]
     time_limit = '10m'
+
+    @run_before('run')
+    def set_arguments(self):
+        # We can't set `executable_opts` in the constructor of the class because
+        # otherwise we hardcode the default value of `num_tasks_per_node`,
+        # overriding a possible setting on the command line.
+        self.executable_opts = [
+            # The `%6N` syntax requires using coreutil's `date`,
+            # but that should be available on most clusters.
+            '$(date +"%s %6N")',
+            str(self.num_tasks_per_node),
+        ]
 
     @run_before('sanity')
     def set_sanity_patterns(self):
