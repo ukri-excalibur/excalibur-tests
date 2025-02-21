3af2d52fce4dace9f6ba00131eaa71c9b01abe3f
Miren Radia <miren_radia@yahoo.co.uk>
2025-02-21 16:31:06 +0000

    Add copying of upstreams.yaml when setting up Spack env
    This allows us to chain our Spack environments to system Spack
    instances. See https://spack.readthedocs.io/en/latest/chain.html.

--- benchmarks/modules/utils.py
+++ benchmarks/modules/utils.py
@@ -266,7 +266,7 @@ class SpackTest(rfm.RegressionTest):
             # Copy Spack environment (only specific YAML files) to the stage
             # directory.
             f'mkdir -p {dest}',
-            f'(cd {cp_dir}; find . \\( -name "spack.yaml" -o -name "compilers.yaml" -o -name "packages.yaml" \\) -print0 | xargs -0 tar cf - | tar -C {dest} -xvf -)',
+            f'(cd {cp_dir}; find . \\( -name "spack.yaml" -o -name "compilers.yaml" -o -name "packages.yaml" -o -name "upstreams.yaml" \\) -print0 | xargs -0 tar cf - | tar -C {dest} -xvf -)',
             f'spack -e {self.build_system.environment} config add "config:install_tree:root:{env_dir}/opt"',
         ]
         cmd_spack_spec_dict =   'from spack import environment;\
