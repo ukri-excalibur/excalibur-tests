13cb20c2129c738c303c58275dff800aa217b0c8
Connor Aird <c.aird@ucl.ac.uk>
2025-02-26 11:52:56 +0000

    Add seperate reframe scripts for running gromacs with cpu and gpu

--- benchmarks/apps/gromacs/gromacs.py
+++ benchmarks/apps/gromacs/gromacs.py
@@ -16,8 +16,7 @@ class GROMACSBenchmark(SpackTest):
     valid_prog_environs = ['default']
     spack_spec = 'gromacs@2019' 
     executable = 'gmx_mpi'
-    executable_opts = ['mdrun', '-noconfout', '-dlb', 'yes', '-s', 'gromacs_1400k_atoms.tpr']
-    time_limit = '120m'
+    time_limit = '240m'
     exclusive_access = True
 
     sourcesdir = os.path.dirname(__file__)
@@ -32,20 +31,20 @@ class GROMACSBenchmark(SpackTest):
     @run_after('setup')
     def setup_variables(self):
         self.num_tasks = self.num_tasks_param
-        self.num_cpus_per_task = self.num_cpus_per_task_param
+        self.num_cpus_per_task = 1
         self.env_vars['OMP_NUM_THREADS'] = f'{self.num_cpus_per_task}'
         self.env_vars['OMP_PLACES'] = 'cores'
 
         if self.current_partition.scheduler.registered_name == 'sge':
             # Set the total number of CPUs to be requested for the SGE scheduler.
             # Set to a full node size to reduce runtime variance.
-            self.extra_resources['mpi'] = {'num_slots': self.current_partition.processor.num_cpus}
+            self.extra_resources['mpi'] = {'num_slots': self.num_tasks}
 
     @run_before('sanity')
     def set_sanity_patterns(self):
         """Set the required string in the output for a sanity check"""
         self.sanity_patterns = sn.assert_found(
-            'Finished mdrun"', self.expected_output_file
+            'Finished mdrun', self.expected_output_file
         )
 
     @run_before('performance')
@@ -56,8 +55,22 @@ class GROMACSBenchmark(SpackTest):
                                      lambda x: float(x.split()[1]))
         }
 
-
 @rfm.simple_test
 class StrongScalingBenchmark(GROMACSBenchmark):
     num_tasks_param = parameter([2, 4, 8, 16, 32])
-    num_cpus_per_task_param = 1
+
+@rfm.simple_test
+class StrongScalingBenchmarkAcrossNodes(GROMACSBenchmark):
+    num_tasks_param = parameter([i * current_partition.processor.num_cpus for i in [1, 2, 3, 4]])
+
+@rfm.simple_test
+class StrongScalingCPUBenchmark(GROMACSBenchmark):
+    num_tasks_param = parameter([i * current_partition.processor.num_cpus for i in [1, 2, 3, 4]])
+    executable_opts = ['mdrun', '-noconfout', '-dlb', 'yes', '-s', 'gromacs_1400k_atoms.tpr']
+
+
+@rfm.simple_test
+class StrongScalingGPUBenchmark(GROMACSBenchmark):
+    num_tasks_param = parameter([i * current_partition.processor.num_cpus for i in [1, 2, 3, 4]])
+    executable_opts = ['mdrun', '-s', 'gromacs_1400k_atoms.tpr', '-nb', 'gpu', '-pme', 'gpu', '-bonded', 'gpu', '-dlb', 'no', '-nstlist', '300', '-pin', 'on', '-v', '-gpu_id', '0']
+
