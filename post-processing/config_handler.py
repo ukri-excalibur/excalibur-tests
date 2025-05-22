f7b08e98f5ed52a3b9a85a935cd6da00db6804e0
pineapple-cat <em.dubrovska@gmail.com>
2025-05-22 19:15:03 +0100

    Removed number of series conditions requirement.

--- post-processing/config_handler.py
+++ post-processing/config_handler.py
@@ -297,8 +297,6 @@ def read_config(config: dict):
 
     # check optional series information
     if config.get("series"):
-        if len(config.get("series")) == 1:
-            raise RuntimeError("Number of series must be >= 2.")
         if len(set([s[0] for s in config.get("series")])) > 1:
             raise RuntimeError("Currently supporting grouping of series by only one column. \
                                Please use a single column name in your series configuration.")
