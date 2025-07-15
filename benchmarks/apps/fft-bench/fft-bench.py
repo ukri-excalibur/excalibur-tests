996779d18add73f91b3c8dd92d8c5476c4a70b5a
Marcus-Keil <marcusk050291@gmail.com>
2025-07-15 15:27:54 +0100

    Changes to the FFT Bench tests and spack package for testing on Myriad

--- benchmarks/apps/fft-bench/fft-bench.py
+++ benchmarks/apps/fft-bench/fft-bench.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import reframe as rfm
 import reframe.utility.sanity as sn
@@ -16,6 +17,7 @@ class FftBenchmarkCpu(SpackTest):
     valid_prog_environs = ['default']
     tasks = parameter([1])  # Used to set `num_tasks` in `__init__`.
     num_tasks_per_node = 1
+    sourcesdir = os.path.dirname(__file__)
     time_limit = '4h'
 
 
